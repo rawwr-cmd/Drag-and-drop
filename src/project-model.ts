ae088fb0785b34f2e3b36c9a58a48e768a5b512c
rawwr <akshayace0302@gmail.com>
2022-10-18 20:26:29 +0530

    working with namespaces

---
+++ src/project-model.ts
@@ -0,0 +1,16 @@
+namespace App {
+  export enum ProjectType {
+    Active,
+    Finished,
+  }
+
+  export class Project {
+    constructor(
+      public id: string,
+      public title: string,
+      public description: string,
+      public people: number,
+      public status: ProjectType
+    ) {}
+  }
+}
